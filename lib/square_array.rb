8c21d9448ee1dbd389ca1c2815d702c02f4aeae7
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-16 19:42:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,11 @@
 def square_array(array)
+count = 0 
 index = 0
 new_array = []
-while  == array.length do 
+while count < array.length do 
   new_array.push(array[index**])
-  
+
+count += 1   
 index += 1 
 end
 new_array
